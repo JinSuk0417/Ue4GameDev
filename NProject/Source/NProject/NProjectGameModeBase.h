ba3622b9f992a9ad045559ee0705e31ca69f96db
JinSuk0417 <shwlstjr345@naver.com>
2021-05-16 12:55:21 +0900

    Init Commit - 첫번째 커밋

---
+++ NProject/Source/NProject/NProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NPROJECT_API ANProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
