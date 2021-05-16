a7dfbf9dbc5abece9ec63cc027ad02fea5be98b1
JinSuk0417 <shwlstjr345@naver.com>
2021-05-16 13:01:01 +0900

    Revert "Init Commit - 첫번째 커밋"
    This reverts commit ba3622b9f992a9ad045559ee0705e31ca69f96db.

--- NProject/Source/NProject/NProject.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "NProject.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NProject, "NProject" );
